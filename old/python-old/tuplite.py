6f24a600056e2e5a759f2cd6fc3272e39395f66b
blob <blob@local.local>
2024-03-09 13:34:37 -0800

    save

---
+++ old/python-old/tuplite.py
@@ -0,0 +1,133 @@
+import sqlite3
+from typing import NamedTuple, List, TypeVar, Generic
+from enum import StrEnum
+
+DB_PATH = "example.db"
+
+
+T = TypeVar("T", bound=NamedTuple)
+
+
+# class TupliteError(StrEnum):
+#     key_exists = "Key already exists"
+
+
+class Storage(Generic[T]):
+    table_name: str
+    named_tuple_type: type[T]
+    db_conn: sqlite3.Connection
+    db_cursor: sqlite3.Cursor
+
+    def __init__(
+        self,
+        table_name: str,
+        named_tuple_type: type[T],
+        primary_key_field: str | None = None,
+        db_conn: sqlite3.Connection | None = None,
+        db_cursor: sqlite3.Cursor | None = None,
+    ) -> None:
+        self.table_name = table_name
+        self.named_tuple_type = named_tuple_type
+
+        self.db_conn = db_conn if not db_conn is None else sqlite3.connect(DB_PATH)
+        self.db_cursor = db_cursor if not db_cursor is None else self.db_conn.cursor()
+
+        self.create_table_if_not_exists(primary_key_field)
+
+    def create_table_if_not_exists(self, primary_key_field: str | None = None):
+        fields = []
+        for field, field_type in self.named_tuple_type.__annotations__.items():
+            field_definition = f"{field} {self.get_field_type(field_type)}"
+            if field == primary_key_field:
+                field_definition += " PRIMARY KEY"
+            fields.append(field_definition)
+        fields_str = ", ".join(fields)
+
+        query = f"CREATE TABLE IF NOT EXISTS {self.table_name} ({fields_str})"
+        self.db_cursor.execute(query)
+
+    def save_item(self, item: T):
+        placeholders = ", ".join(
+            ["?" for _ in range(len(self.named_tuple_type._fields))]
+        )
+        fields = ", ".join(self.named_tuple_type._fields)
+        query = f"INSERT INTO {self.table_name} ({fields}) VALUES ({placeholders})"
+        values = tuple(getattr(item, field) for field in self.named_tuple_type._fields)
+        self.db_cursor.execute(query, values)
+        self.db_conn.commit()
+
+    def get_item(self, **kwargs) -> T | None:
+        conditions = " AND ".join([f"{field} = ?" for field, value in kwargs.items()])
+        query = f"SELECT * FROM {self.table_name} WHERE {conditions}"
+        values = tuple(kwargs.values())
+        self.db_cursor.execute(query, values)
+        row = self.db_cursor.fetchone()
+        if row:
+            return self.named_tuple_type(*row)
+        else:
+            return None
+
+    def delete_item(self, **kwargs):
+        conditions = " AND ".join([f"{field} = ?" for field, value in kwargs.items()])
+        query = f"DELETE FROM {self.table_name} WHERE {conditions}"
+        values = tuple(kwargs.values())
+        self.db_cursor.execute(query, values)
+        self.conn.commit()
+
+    def list_entries(self) -> List[T]:
+        query = f"SELECT * FROM {self.table_name}"
+        self.db_cursor.execute(query)
+        rows = self.db_cursor.fetchall()
+        entries = [self.named_tuple_type(*row) for row in rows]
+        return entries
+
+    def update_item(self, item: T):
+        fields = ", ".join([f"{field} = ?" for field in self.named_tuple_type._fields])
+        query = f"UPDATE {self.table_name} SET {fields} WHERE {self.named_tuple_type._fields[0]} = ?"
+        values = tuple(
+            getattr(item, field) for field in self.named_tuple_type._fields
+        ) + (
+            getattr(item, self.named_tuple_type._fields[0]),
+        )
+        self.db_cursor.execute(query, values)
+        self.db_conn.commit()
+
+    @staticmethod
+    def get_field_type(field_type):
+        if field_type == str:
+            return "TEXT"
+        elif field_type == int:
+            return "INTEGER"
+        elif field_type == float:
+            return "REAL"
+        else:
+            raise ValueError(f"Unsupported field type: {field_type}")
+
+
+# # testing:
+
+
+# class Person(NamedTuple):
+#     name: str
+#     age: float
+#     email: str
+
+
+# storage = Storage[Person]("persons", Person, primary_key_field="name")
+
+# person1 = Person("John Doe", 30.234, "john@example.com")
+# person2 = Person("Jane Smith", 25, "jane@example.com")
+
+
+# storage.save_item(person1)
+# storage.save_item(person2)
+
+# retrieved_person = storage.get_item(name="John Doe")
+# print(retrieved_person)
+# # # Output: Person(name='John Doe', age=30, email='john@example.com')
+
+# # storage.delete_item(name="Jane Smith")  # delete where
+
+# persons = storage.list_entries()
+# print(persons)
+# # Output: [Person(name='John Doe', age=30, email='john@example.com')]
---
+++ old/typescript-old/dbinterface.ts
@@ -0,0 +1,136 @@
+// DBInterface Alpha 0.2 by blobbybilb (from 7.Oct.2023)
+
+import { Database } from "bun:sqlite"
+
+// type validTypes = string | number | boolean | null
+type validTypes = string | number
+export type Item = Record<string, validTypes>
+type query<T> =
+  { [K in keyof T as string extends K ? never : K]: T[K] }
+// [P in keyof query<T>]?: ((arg: query<T>[P]) => boolean) <--- this goes as the parameter type
+
+export class DBInterface {
+  dbPath: string
+  db: Database
+
+  constructor(path: string | undefined = undefined) {
+    this.dbPath = path ?? ":memory:"
+    this.db = new Database(this.dbPath, { create: true })
+  }
+
+  openTable<T extends Item>(table: string): DBTable<T> {
+    return new DBTable<T>(this.db, table)
+  }
+
+  listTables(): string[] {
+    return this.db.query("SELECT name FROM sqlite_master WHERE type='table'").all().map((e: any) => e.name) as string[]
+  }
+
+  deleteTable(table: string) {
+    this.db.query(`DROP TABLE ${table}`).run()
+  }
+}
+
+
+class DBTable<T extends Item> {
+  db: Database
+  table: string
+  // itemType: T | undefined
+  tableExists: boolean
+
+  constructor(db: Database, table: string) {
+    this.db = db
+    this.table = table
+    this.tableExists = this.db.query("SELECT name FROM sqlite_master WHERE type='table' AND name=?").get(table) ? true : false
+  }
+
+  getSQLType(value: validTypes): string {
+    if (typeof value === "string") {
+      return "TEXT"
+    } if (typeof value === "number") {
+      return "REAL"
+    } if (typeof value === "boolean") {
+      return "INTEGER"
+    } if (value === null) {
+      return "NULL"
+    }
+    else {
+      throw new Error("Invalid type")
+    }
+  }
+
+  getRowType(item: Item): string[] {
+    return Object.entries(item).map(([_, value]) => this.getSQLType(value))
+  }
+
+  createTable(item: T) {
+    const rowType = this.getRowType(item)
+    const rowNamesString = Object.keys(item).map((name, index) => `${name} ${rowType[index]}`).join(", ")
+    this.db.query(`CREATE TABLE IF NOT EXISTS ${this.table} (${rowNamesString})`).run()
+    this.tableExists = true
+  }
+
+  add(item: T) {
+    if (!this.tableExists) this.createTable(item)
+
+    const rowNames = Object.keys(item)
+    const rowValuesString = Object.values(item).map((value) => typeof value === "string" ? `'${value}'` : value).join(", ")
+    const rowNamesString = rowNames.join(", ")
+    this.db.query(`INSERT INTO ${this.table} (${rowNamesString}) VALUES (${rowValuesString})`).run()
+  }
+
+  get(query: { [P in keyof query<T>]?: ((arg: query<T>[P]) => boolean) | T[P] } | Item): T[] {
+    let queryResult: T[]
+
+    if ((Object.values(query).filter((value) => typeof value !== "function") as validTypes[]).length === 0) {
+      queryResult = this.db.query(`SELECT * FROM ${this.table}`).all() as T[]
+    } else {
+      const queryKeys = Object.entries(query)
+        .filter(([_, value]) => typeof value !== "function")
+        .map(([key, _]) => `${key}`)
+      const queryValuesString = Object.values(query)
+        .filter((value) => typeof value !== "function")
+        .map((value) => typeof value === "string" ? `'${value}'` : value).join(", ")
+
+      const queryKeysString = queryKeys.join(", ")
+      queryResult = this.db.query(`SELECT * FROM ${this.table} WHERE (${queryKeysString}) = (${queryValuesString})`).all() as T[]
+    }
+
+    for (const [key, value] of Object.entries(query)) {
+      if (typeof value === "function") {
+        queryResult = queryResult.filter((item) => value(item[key]))
+      }
+    }
+
+    return queryResult
+  }
+
+  del(item: Item) {
+    const queryKeys = Object.keys(item)
+    const queryValuesString = Object.values(item).map((value) => typeof value === "string" ? `'${value}'` : value).join(", ")
+
+    if (queryKeys.length === 0) {
+      this.db.query(`DELETE FROM ${this.table}`).run()
+      return
+    }
+
+    const queryKeysString = queryKeys.join(", ")
+    this.db.query(`DELETE FROM ${this.table} WHERE ${queryKeysString} = (${queryValuesString})`).run()
+  }
+
+  update(item: Item, newItem: Item) {
+    const oldItems = this.get(item)
+    this.del(item)
+    for (const oldItem of oldItems) {
+      this.add({ ...oldItem, ...newItem })
+    }
+  }
+}
+
+const db = new DBInterface(":memory:")
+const config = db.openTable<{ name: string, user: string, pass: string }>("config")
+config.add({ name: "test", user: "test", pass: "test" })
+console.log(config.get({ name: "test" }))
+config.update({ name: "test" }, { name: "test", user: "test2" })
+console.log(config.get({ name: "test" }))
+config.del({ name: "test" })
