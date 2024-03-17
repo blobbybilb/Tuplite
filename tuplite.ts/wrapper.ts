59bc63f0775943dda728ddf8eca8a773c2cdb19e
blobbybilb <notablob@outlook.com>
2024-03-17 15:46:29 -0700

    Update wrapper.ts

--- tuplite.ts/wrapper.ts
+++ tuplite.ts/wrapper.ts
@@ -1,7 +1,8 @@
 abstract class SQLiteWrapper {
   abstract runQuery(sql: string): void
   abstract listTables(): string[]
-  tableExists = (table: string) => this.listTables().includes(table)
+  tableExists: (table: string) => boolean = (table) =>
+    this.listTables().includes(table)
   abstract getAs<T>(query: string): T[]
 }
 
